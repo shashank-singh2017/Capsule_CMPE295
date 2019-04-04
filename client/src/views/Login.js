395af629f2dc26f643392ca71f5ab54f77e26564
shashank-singh2017 <shashank.singh@sjsu.edu>
2019-04-04 08:23:09 -0700

    added admin-dashboard pages

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import "../stylesheets/login-signup.css";
 import Header from "./Header";
-import web3 from "../web3";
-import capsuleUser from "../capsuleUser";
+// import web3 from "../web3";
+// import capsuleUser from "../capsuleUser";
 
-const myAccount = "0x9b6E8A3366084aae75ceC8b4348B8281080949f9";
+// const myAccount = "0x9b6E8A3366084aae75ceC8b4348B8281080949f9";
 
 class Login extends Component {
   state = {
@@ -19,43 +19,43 @@ class Login extends Component {
     address: ""
   };
 
-  async componentDidMount() {
-    // const manager = await lottery.methods.manager().call();
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-      userType
-    } = await capsuleUser.methods.getUser(myAccount).call();
-    // const balance = await web3.eth.getBalance(capsuleUser.options.address);
-    this.setState({ firstName, lastName, email, password, userType });
-  }
+  // async componentDidMount() {
+  //   // const manager = await lottery.methods.manager().call();
+  //   const {
+  //     firstName,
+  //     lastName,
+  //     email,
+  //     password,
+  //     userType
+  //   } = await capsuleUser.methods.getUser(myAccount).call();
+  //   // const balance = await web3.eth.getBalance(capsuleUser.options.address);
+  //   this.setState({ firstName, lastName, email, password, userType });
+  // }
 
   onSubmit = async event => {
     event.preventDefault();
 
-    const accounts = await web3.eth.getAccounts();
+    // const accounts = await web3.eth.getAccounts();
 
     this.setState({
       message: "Trying to login...",
       value: "0.01"
     });
 
-    console.log(this.state.address);
+    // console.log(this.state.address);
 
-    const user = await capsuleUser.methods.getUser(this.state.address).call();
+    // const user = await capsuleUser.methods.getUser(this.state.address).call();
 
-    console.log(user);
+    // console.log(user);
 
-    this.setState({
-      firstName: user[0],
-      lastName: user[1],
-      email: user[2],
-      password: user[3],
-      userType: user[4],
-      message: "You have succefully logged in!"
-    });
+    // this.setState({
+    //   firstName: user[0],
+    //   lastName: user[1],
+    //   email: user[2],
+    //   password: user[3],
+    //   userType: user[4],
+    //   message: "You have succefully logged in!"
+    // });
   };
 
   render() {
